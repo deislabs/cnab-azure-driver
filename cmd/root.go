64cfaadfd9ee991f3139a5eb50b018dc79bede8f
Simon Davies <simongdavies@users.noreply.github.com>
2019-05-13 22:09:46 +0100

    Moved to standalone command driver

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 
+<<<<<<< HEAD
 	"github.com/deislabs/duffle-aci-driver/pkg"
 	"github.com/deislabs/duffle-aci-driver/pkg/driver"
 
@@ -12,83 +12,54 @@ import (
 	"github.com/spf13/cobra"
 
 	"io/ioutil"
+=======
+	duffleDriver "github.com/deislabs/cnab-go/driver"
+
+	"github.com/deislabs/duffle-aci-driver/pkg"
+	"github.com/deislabs/duffle-aci-driver/pkg/driver"
+
+	"github.com/spf13/cobra"
+
+>>>>>>> Moved to standalone command driver
 	"os"
 )
 
-// Version is the current version of duffle-aci-driver
-var Version string
-
 var handles bool
-
+var op duffleDriver.Operation
 var rootCmd = &cobra.Command{
-	Use:          "duffle-aci-driver",
-	Short:        "duffle-aci-driver is a duffle driver to execute CNAB actions",
-	Long:         `A duffle driver to execute CNAB actions using Azure ACI`,
-	RunE:         runRootCmd,
-	SilenceUsage: true,
-}
+	Use:   "duffle-aci-driver",
+	Short: "duffle-aci-driver is a duffle driver to execute CNAB actions",
+	Long:  `duffle-aci-driver is a duffle driver to execute CNAB actions using Azure ACI`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if handles {
+			fmt.Printf("%s,%s\n", duffleDriver.ImageTypeDocker, duffleDriver.ImageTypeOCI)
+		} else {
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the aci driver version",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("duffle-aci-driver version:%v \n", Version)
-	},
-}
+			fi, err := os.Stdin.Stat()
+			if err != nil {
+				return fmt.Errorf("cannot stat stdin: %v", err)
+			}
 
-func runRootCmd(cmd *cobra.Command, args []string) error {
+			if fi.Size() > 0 {
+				err := json.NewDecoder(os.Stdin).Decode(&op)
+				if err != nil {
+					return err
+				}
+				acidriver := getDriver()
+				if configurable, ok := acidriver.(duffleDriver.Configurable); ok {
+					driverCfg := map[string]string{}
+					for env := range configurable.Config() {
+						driverCfg[env] = os.Getenv(env)
+					}
+					configurable.SetConfig(driverCfg)
+				}
+				return acidriver.Run(&op)
+			}
+			cmd.Usage()
 
-	if handles {
-		HandlesImageTypes()
-		return nil
-	}
-
-	op, err := GetOperation()
-	if err != nil {
-		return err
-	}
-
-	acidriver := getDriver()
-	if configurable, ok := acidriver.(cnabdriver.Configurable); ok {
-		driverCfg := map[string]string{}
-		for env := range configurable.Config() {
-			driverCfg[env] = os.Getenv(env)
 		}
-		configurable.SetConfig(driverCfg)
-	}
-
-	fmt.Printf("Running %s action on %s\n", op.Action, op.Installation)
-	return acidriver.Run(op)
-
-}
-
-// HandlesImageTypes writes output containing comma seperated values list of imageTypes that the ACI Driver can handle
-func HandlesImageTypes() {
-	fmt.Printf("%s,%s\n", cnabdriver.ImageTypeDocker, cnabdriver.ImageTypeOCI)
-}
-
-// GetOperation gets the Operation to execute
-func GetOperation() (*cnabdriver.Operation, error) {
-	var op cnabdriver.Operation
-	fi, err := os.Stdin.Stat()
-	if err != nil {
-		return nil, fmt.Errorf("Error getting FileInfo for stdin: %v", err)
-	}
-
-	if fi.Size() == 0 {
-		return nil, errors.New("No input passed on stdin")
-	}
-
-	bytes, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading from stdin: %v", err)
-	}
-
-	if err = json.Unmarshal(bytes, &op); err != nil {
-		return nil, fmt.Errorf("Error getting bundle.json: %v", err)
-	}
-
-	return &op, nil
+		return nil
+	},
 }
 
 func init() {
--- main.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main() {
-	println("aci vroom vroom 🏎")
-}
