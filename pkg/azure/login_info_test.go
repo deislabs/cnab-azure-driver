84eeb9368f764e8603b3cf5b8646559b9410f45e
Simon Davies <simongdavies@users.noreply.github.com>
2019-09-18 18:25:54 +0100

    updated login info test

--- pkg/azure/login_info_test.go
+++ pkg/azure/login_info_test.go
@@ -15,7 +15,7 @@ func TestGetLoginInfo(t *testing.T) {
 		envVarsToGet []string
 		valueToCheck LoginType
 	}{
-		// Set environments vars using TEST_
+		// to execute tests set environment vars using names in envVarsToGet preceded with TEST_
 		{"Get LoginInfo using Service Principal", []string{"DUFFLE_ACI_DRIVER_CLIENT_ID", "DUFFLE_ACI_DRIVER_CLIENT_SECRET", "DUFFLE_ACI_DRIVER_TENANT_ID"}, ServicePrincipal},
 		{"Get LoginInfo using Service Principal", []string{"DUFFLE_ACI_DRIVER_APP_ID"}, DeviceCode},
 	}
